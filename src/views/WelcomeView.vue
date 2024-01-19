c8cc2c51e859c0640510332b9b58feafb56d6410
ES <29315@Arutoria.com>
2024-01-19 15:38:57 +0800

    完成路由和element plus配置

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex:1;background-color: black">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+          src="src/image/RE4wtbD.jpg"/>
+
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到医疗管理系统</div>
+      <div style="margin-top: 10px">在这里你可以找到各种医疗设施</div>
+      <div style="margin-top: 5px">在这里你可以找到各个学校的医疗团队</div>
+    </div>
+    <div class="right-card">
+      <router-view/>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 30px black;
+}
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: white;
+}
+</style>
---
+++ src/views/welcome/LoginPage.vue
@@ -0,0 +1,32 @@
+<script setup>
+const form = reactive({
+  username : '',
+  password : '',
+  remember: false,
+
+});
+</script>
+
+<template>
+
+  <div style="text-align:center;margin: 0 20px">
+    <div style="margin-top: 150px">
+      <div style="font-size: 25px;font-weight: bold">登录</div>
+      <div style="font-size: 14px;color: grey">在进入系统前请先输入用户名和密码登录</div>
+    </div>
+    <div style="margin-top: 50px">
+      <el-form v-model="form">
+        <el-form-item>
+          <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名/邮箱">
+          </el-input>
+        </el-form-item>
+      </el-form>
+
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
